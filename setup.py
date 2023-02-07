4a521088f03b5fecfb5a496a4817863d6d1bf7a9
Tim Jonas Meinerzhagen <34220101+timmeinerzhagen@users.noreply.github.com>
2023-02-07 21:12:30 +0100

    chore: update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mkdocs-link-marker',
-    version='0.1.1',
+    version='0.1.2',
     description='MkDocs plugin for marking external or mail links in your documentation.',
     long_description=long_description,
     long_description_content_type="text/markdown",
