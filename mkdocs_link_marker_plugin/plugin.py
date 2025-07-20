7f828bef96bad4e462edbab8c726b36b01c755d6
Tim Jonas Meinerzhagen <34220101+timmeinerzhagen@users.noreply.github.com>
2025-07-20 04:08:44 +0200

    feat: ignore external link when image (#13)

--- mkdocs_link_marker_plugin/plugin.py
+++ mkdocs_link_marker_plugin/plugin.py
@@ -21,7 +21,13 @@ class LinkMarkerPlugin(BasePlugin):
     def on_page_content(self, html, page, config, files):
         if self.config['enable_external_link']:
             p_external_link = re.compile('<a href="(http.*?)">(.*?)</a>')
-            html = p_external_link.sub('<a href="\\1">\\2&nbsp;' + self.config['icon_external_link'] + '</a>', html)
+            def repl_external_link(match):
+                href, content = match.group(1), match.group(2)
+                # Do not add icon if the content contains an <img> tag
+                if re.search(r'<img\b', content, re.IGNORECASE):
+                    return f'<a href="{href}">{content}</a>'
+                return f'<a href="{href}">{content}&nbsp;{self.config["icon_external_link"]}</a>'
+            html = p_external_link.sub(repl_external_link, html)
 
         if self.config['enable_mail']:
             p_mail = re.compile('<a href="(mailto:.*?)">(.*?)</a>')
